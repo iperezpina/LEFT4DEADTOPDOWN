87441035ac0ba600e8a0412afff41aba1507e205
Ivan Perez Pina <46605900+piggy145@users.noreply.github.com>
2020-06-02 14:49:59 -0700

    Add files via upload

---
+++ Camera/Script4Cam/TopDownCamer.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class TopDownCamer : MonoBehaviour
+{
+    public Transform m_Target;
+    public float m_height = 10f;
+    public float m_Distance = 20f;
+    public float m_Angle = 45f;
+    public float m_SmoothSpeed = 0.5f;
+
+    private Vector3 refVolocity;
+    // Start is called before the first frame update
+    void Start()
+    {
+        HandleCamera();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleCamera();
+    }
+
+
+
+    protected virtual void HandleCamera()
+    {
+        if (!m_Target)
+        {
+            return;
+        }
+       
+        Vector3 worldPostion = (Vector3.forward * -m_Distance) +(Vector3.up * m_height);
+        //Debug.DrawLine(m_Target.position, worldPostion, Color.red);
+
+
+
+        Vector3 rotatedVector = Quaternion.AngleAxis(m_Angle, Vector3.up) * worldPostion;
+        
+        Vector3 flatTargetPos = m_Target.position;
+        flatTargetPos.y = 0f;
+        Vector3 finalPostion = flatTargetPos + rotatedVector;
+
+        transform.position = Vector3.SmoothDamp(transform.position, finalPostion, ref refVolocity, m_SmoothSpeed);
+        transform.LookAt(flatTargetPos);
+        
+    }
+
+
+}
---
+++ Gun4Player/Scripts/GunController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunController : MonoBehaviour
+{
+    public Transform weaponHold;
+    public Gun startGun;
+    Gun equippedGun;
+
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (startGun != null) {
+            EquipGun(startGun);            
+        }        
+
+
+    }
+
+
+    public void EquipGun(Gun gunToEquip)
+    {
+        if (equippedGun != null)
+        {
+            Destroy(equippedGun.gameObject);
+
+        }
+        equippedGun = Instantiate(gunToEquip, weaponHold.position, weaponHold.rotation) as Gun;
+        equippedGun.transform.parent = weaponHold;
+
+    }
+
+    public void Shoot()
+    {
+        if (equippedGun != null)
+        {
+            equippedGun.Shoot();
+        }
+    }
+
+}
